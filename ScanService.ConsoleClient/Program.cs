a22adf5421f1b507a559e079a67d0be16cc0b2e5
Tsypin Ilya <ilyaboy@list.ru>
2022-06-12 13:15:28 +0300

    Add startScanService

--- ScanService.ConsoleClient/Program.cs
+++ ScanService.ConsoleClient/Program.cs
@@ -1,9 +1,80 @@
-﻿namespace ScanService.ConsoleClient;
+﻿using System.Diagnostics;
+
+namespace ScanService.ConsoleClient;
 
 public static class Program
 {
+    private const string Api = "https://localhost:5001/";
+    private static readonly HttpClient _client = new();
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello world!");
+        var input = Console.ReadLine()?.ToLower();
+
+        while (input != "exit")
+        {
+            if (input == "scan_service")
+            {
+                StartScanService();
+            }
+            else
+            {
+                try
+                {
+                    var attribute = input.Split(" ")[2];
+
+                    if (!string.IsNullOrEmpty(attribute))
+                    {
+                        if (input.Contains("scan_util scan "))
+                        {
+                            CreateScan(attribute);
+                        }
+                        else if (input.Contains("scan_util status "))
+                        {
+                            GetScanStatus(attribute);
+                        }
+                        else
+                        {
+                            Console.WriteLine("There is no such command!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no such command!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There is no such command!");
+                }
+            }
+
+            input = Console.ReadLine()?.ToLower();
+        }
+    }
+
+    private static void StartScanService()
+    {
+        Console.WriteLine("Scan service was started.");
+        
+        Process.Start(Path.Combine(
+            Directory.GetParent(Directory.GetCurrentDirectory())
+                .Parent.Parent.Parent.FullName, 
+            @"ScanService.API\bin\Debug\net6.0\ScanService.API.exe"
+            ));
+        
+        _client.BaseAddress = new Uri(Api);
+        
+        Console.WriteLine("Write <Exit> to exit...");
+    }
+
+    private static void CreateScan(string directory)
+    {
+        
+    }
+
+    private static void GetScanStatus(string id)
+    {
+        
     }
 }
